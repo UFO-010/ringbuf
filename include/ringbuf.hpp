c2390a1a41f2ebf21c567bc88622c4ac3c989d40
UFO-010 <smeliy.kirill2014@yandex.ru>
2025-12-22 14:08:13 +0300

    Fixed unsubscribe

--- include/ringbuf.hpp
+++ include/ringbuf.hpp
@@ -431,8 +431,8 @@ public:
             return false;
         }
 
-        callbacks[index] = std::move(callbacks[callback_count]);
         callback_count--;
+        callbacks[index] = std::move(callbacks[callback_count]);
         return true;
     }
 
